ec7c31593a794e100227077984a1b6715ea1f5d6
007yukio <tenten0407team@yahoo.co.jp>
2018-07-01 08:44:05 +0000

    sample-01

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   
-  get 'blogs' => 'blogs#index'
   resources :blogs do
     collection do
       post :confirm  
     end
   end
+  
+  root to: 'blogs#new'
+  
 end
